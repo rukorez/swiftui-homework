aacf77cacaaf4ca0810f50bc53b059b120ff1f4a
rukorez <77271521+rukorez@users.noreply.github.com>
2022-11-30 17:15:44 +0300

    Initial Commit

---
+++ SwiftUITestProject/SwiftUITestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestProjectApp.swift
+//  SwiftUITestProject
+//
+//  Created by Филипп Степанов on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
